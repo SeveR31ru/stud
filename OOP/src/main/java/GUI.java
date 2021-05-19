87f2d74f69af2c874a6182ec2964adbbf9198841
Leha009 <aleshay1@mail.com>
2021-05-19 12:58:20 +0300

    GUI updated

--- OOP/src/main/java/GUI.java
+++ OOP/src/main/java/GUI.java
@@ -45,6 +45,8 @@ class MainMenu
     {
         this.rHandler = new RaceHandler();
         rHandler.loadData();
+
+        PrepareTables();
         mainFrame = new JFrame();
         mainFrame.setTitle("Races manager");
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -62,7 +64,7 @@ class MainMenu
             event -> {
                 System.out.println("Racers here");
                 if(racerViewer == null)
-                    racerViewer = new RacerViewer(mainFrame, rHandler.getRacers());
+                    racerViewer = new RacerViewer(mainFrame, rHandler.getRacers(), racersPane, true);
                 racerViewer.setVisible(true);
             }
         );
@@ -72,7 +74,7 @@ class MainMenu
             event -> {
                 System.out.println("Teams here");
                 if(teamViewer == null)
-                    teamViewer = new TeamViewer(mainFrame, rHandler.getTeams());
+                    teamViewer = new TeamViewer(mainFrame, rHandler.getTeams(), teamsPane, true);
                 teamViewer.setVisible(true);
             }
         );
@@ -82,7 +84,7 @@ class MainMenu
             event -> {
                 System.out.println("Tracks here");
                 if(trackViewer == null)
-                    trackViewer = new TrackViewer(mainFrame, rHandler.getTracks());
+                    trackViewer = new TrackViewer(mainFrame, rHandler.getTracks(), tracksPane, true);
                 trackViewer.setVisible(true);
             }
         );
@@ -92,7 +94,7 @@ class MainMenu
             event -> {
                 System.out.println("Competitions here");
                 if(compViewer == null)
-                    compViewer = new CompViewer(mainFrame, rHandler.getComps());
+                    compViewer = new CompViewer(mainFrame, rHandler.getComps(), compsPane, true);
                 compViewer.setVisible(true);
             }
         );
@@ -111,11 +113,9 @@ class MainMenu
 
         mainFrame.pack();
         mainFrame.setVisible(true);
-
-        PrepareTable();
     }
 
-    private void PrepareTable()
+    private void PrepareTables()
     {
         //============================RACERS============================//
         String[] racersColumns = {"ID", "NAME", "SURNAME", "TEAM", "SCORE"};
@@ -124,8 +124,9 @@ class MainMenu
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 if(columnIndex == 3)
                 {
-                    Racer toPrint = rHandler.getRacers().get(rowIndex);
-                    System.out.println("Show " + toPrint.getName() + "'s team");
+                    showObj(rHandler.getRacers().get(rowIndex).getTeam());
+                    /*Racer toPrint = rHandler.getRacers().get(rowIndex);
+                    System.out.println("Show " + toPrint.getName() + "'s team");*/
                 }
                 return false;
             }
@@ -159,7 +160,9 @@ class MainMenu
             @Override
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 if(columnIndex == 2)
-                    System.out.println("Show racers in the team");
+                {
+                    showObjs(rHandler.getTeams().get(rowIndex).getRacers());
+                }
                 return false;
             }
         };
@@ -219,11 +222,33 @@ class MainMenu
             @Override
             public boolean isCellEditable(int rowIndex, int columnIndex) {
                 if(columnIndex == 3)
-                    System.out.println("Show track's info");
+                {
+                    showObj(rHandler.getComps().get(rowIndex).getTrack());
+                    /*Track trackToShow = rHandler.getComps().get(rowIndex).getTrack();
+                    List<Track>trackList = new ArrayList<Track>();
+                    trackList.add(trackToShow);
+                    TrackViewer trackShown = new TrackViewer(mainFrame, trackList, getPane(trackList) ,false);
+                    trackShown.setVisible(true);
+                    System.out.println("Show track's info");*/
+                }
                 else if(columnIndex == 4)
-                    System.out.println("Show racers in the competition");
+                {
+                    showObjs(rHandler.getComps().get(rowIndex).getRacers());
+                    /*Competition comp = rHandler.getComps().get(rowIndex);
+                    RacerViewer racersInCompViewer = new RacerViewer(mainFrame, comp.getRacers(), getPane(comp.getRacers()), false);
+                    racersInCompViewer.setVisible(true);
+                    System.out.println("Show racers in the competition");*/
+                }
                 else if(columnIndex == 5)
-                    System.out.println("Show winner in the competition");
+                {
+                    showObj(rHandler.getComps().get(rowIndex).getWinner());
+                    /*Competition comp = rHandler.getComps().get(rowIndex);
+                    List<Racer>racerList = new ArrayList<Racer>();
+                    racerList.add(comp.getWinner());
+                    RacerViewer winnerViewer = new RacerViewer(mainFrame, racerList, getPane(racerList), false);
+                    winnerViewer.setVisible(true);
+                    System.out.println("Show winner in the competition");*/
+                }
                 return false;
             }
         };
@@ -256,17 +281,235 @@ class MainMenu
         compsPane.setPreferredSize(new Dimension(780, 380));
     }
 
+    private <T> JScrollPane getPane(List<T> objs)  //Using for find
+    {
+        JScrollPane jPane;
+        DefaultTableModel tModel;
+        JTable table;
+        Dimension dimension;
+        if(objs.get(0).getClass() == Racer.class)
+        {
+            dimension = new Dimension(630, 380);
+            String[] racersColumns = {"ID", "NAME", "SURNAME", "TEAM", "SCORE"};
+            tModel = new DefaultTableModel(){
+                @Override
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    if(columnIndex == 3)
+                    {
+                        Racer toPrint = (Racer)objs.get(rowIndex);
+                        showObj(toPrint.getTeam());
+                    }
+                    return false;
+                }
+            };
+            tModel.setColumnIdentifiers(racersColumns);
+            String[] racerInfo = new String[5];
+            for(int i = 0; i < objs.size(); i++)
+            {
+                Racer racer = (Racer)objs.get(i);
+                racerInfo[0] = ("" + racer.getId());
+                racerInfo[1] = (racer.getName());
+                racerInfo[2] = (racer.getSurname());
+                racerInfo[3] = (racer.getTeam().getName() + " (ID " + racer.getTeam().getId() + ")");
+                racerInfo[4] = ("" + racer.getScore());
+                tModel.addRow(racerInfo);
+            }
+        }
+        else if(objs.get(0).getClass() == Team.class)
+        {
+            dimension = new Dimension(530, 380);
+            String[] teamsColumns = {"ID", "NAME", "RACERS IN TEAM"};
+            tModel = new DefaultTableModel(){
+                @Override
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    if(columnIndex == 2)
+                    {
+                        Team team = (Team)objs.get(rowIndex);
+                        showObjs(team.getRacers());
+                    }
+                    return false;
+                }
+            };
+            tModel.setColumnIdentifiers(teamsColumns);
+            String[] teamInfo = new String[3];
+            for(int i = 0; i < objs.size(); i++)
+            {
+                Team team = (Team)objs.get(i);
+                teamInfo[0] = ("" + team.getId());
+                teamInfo[1] = (team.getName());
+                teamInfo[2] = ("" + team.getRacers().size());
+                tModel.addRow(teamInfo);
+            }
+        }
+        else if(objs.get(0).getClass() == Track.class)
+        {
+            String[] tracksColumns = {"ID", "NAME", "COUNTRY"};
+            tModel = new DefaultTableModel(){
+                @Override
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    return false;
+                }
+            };
+            tModel.setColumnIdentifiers(tracksColumns);
+            String[] trackInfo = new String[3];
+            for(int i = 0; i < objs.size(); i++)
+            {
+                Track track = (Track)objs.get(i);
+                trackInfo[0] = ("" + track.getId());
+                trackInfo[1] = (track.getName());
+                trackInfo[2] = (track.getCountry());
+                tModel.addRow(trackInfo);
+            }
+            dimension = new Dimension(530, 380);
+        }
+        else    //Competition.class
+        {
+            String[] compsColumns = {"ID", "NAME", "DATE", "TRACK", "RACERS", "WINNER"};
+            tModel = new DefaultTableModel(){
+                @Override
+                public boolean isCellEditable(int rowIndex, int columnIndex) {
+                    if(columnIndex == 3)
+                    {
+                        Competition comp = (Competition)objs.get(rowIndex);
+                        showObj(comp.getTrack());
+                        /*Track trackToShow = comp.getTrack();
+                        List<Track>trackList = new ArrayList<Track>();
+                        trackList.add(trackToShow);
+                        TrackViewer trackShown = new TrackViewer(mainFrame, trackList, getPane(trackList), false);
+                        trackShown.setVisible(true);
+                        System.out.println("Show track's info");*/
+                    }
+                    else if(columnIndex == 4)
+                    {
+                        Competition comp = (Competition)objs.get(rowIndex);
+                        showObjs(comp.getRacers());
+                        /*RacerViewer racersInCompViewer = new RacerViewer(mainFrame, comp.getRacers(), getPane(comp.getRacers()), false);
+                        racersInCompViewer.setVisible(true);
+                        System.out.println("Show racers in the competition");*/
+                    }
+                    else if(columnIndex == 5)
+                    {
+                        Competition comp = (Competition)objs.get(rowIndex);
+                        showObj(comp.getWinner());
+                        /*List<Racer>racerList = new ArrayList<Racer>();
+                        racerList.add(comp.getWinner());
+                        RacerViewer winnerViewer = new RacerViewer(mainFrame, racerList, getPane(racerList), false);
+                        winnerViewer.setVisible(true);
+                        System.out.println("Show winner in the competition");*/
+                    }
+                    return false;
+                }
+            };
+            tModel.setColumnIdentifiers(compsColumns);
+            String[] compInfo = new String[6];
+            for(int i = 0; i < objs.size(); i++)
+            {
+                Competition comp = (Competition)objs.get(i);
+                compInfo[0] = ("" + comp.getId());
+                compInfo[1] = (comp.getName());
+                compInfo[2] = ("" + comp.getDate());
+                compInfo[3] = (comp.getTrack().getName());
+                compInfo[4] = (""+ comp.getRacers().size());
+                compInfo[5] = (""+ comp.getWinner().getName() + " " + comp.getWinner().getSurname() + " (ID " + comp.getWinner().getId() + ")");
+                compsModel.addRow(compInfo);
+            }
+            dimension = new Dimension(780, 380);
+        }
+        table = new JTable(tModel);
+        table.getColumnModel().getColumn(0).setMaxWidth(100);
+        table.setEnabled(true);
+        table.setAutoCreateRowSorter(true);    //сортировка в одну строку? Там можно было что ли?!?!?!
+        jPane = new JScrollPane(table);
+        jPane.setPreferredSize(dimension);
+        return jPane;
+    }
+
+    private <T> void showObj(T obj)
+    {
+        if(obj.getClass() == Racer.class)
+        {
+            List<Racer>rList = new ArrayList<>();
+            rList.add( (Racer)obj );
+            RacerViewer rViewer = new RacerViewer(mainFrame, rList, getPane(rList), false);
+            rViewer.setVisible(true);
+            System.out.println("Show racer here");
+        }
+        else if(obj.getClass() == Team.class)
+        {
+            List<Team>tList = new ArrayList<>();
+            tList.add( (Team)obj );
+            TeamViewer tViewer = new TeamViewer(mainFrame, tList, getPane(tList), false);
+            tViewer.setVisible(true);
+            System.out.println("Show team here");
+        }
+        else if(obj.getClass() == Track.class)
+        {
+            List<Track>tList = new ArrayList<>();
+            tList.add( (Track)obj );
+            TrackViewer tViewer = new TrackViewer(mainFrame, tList, getPane(tList), false);
+            tViewer.setVisible(true);
+            System.out.println("Show track here");
+        }
+        else
+        {
+            List<Competition>cList = new ArrayList<>();
+            cList.add( (Competition)obj );
+            CompViewer cViewer = new CompViewer(mainFrame, cList, getPane(cList), false);
+            cViewer.setVisible(true);
+            System.out.println("Show competition here");
+        }
+    }
+
+    private <T> void showObjs(List<T> objs)
+    {
+        if(objs.get(0).getClass() == Racer.class)
+        {
+            RacerViewer rViewer = new RacerViewer(mainFrame, (List<Racer>)objs, getPane((List<Racer>)objs), false);
+            rViewer.setVisible(true);
+            System.out.println("Show racers here");
+        }
+        else if(objs.get(0).getClass() == Team.class)
+        {
+            TeamViewer tViewer = new TeamViewer(mainFrame, (List<Team>)objs, getPane((List<Team>)objs), false);
+            tViewer.setVisible(true);
+            System.out.println("Show team here");
+        }
+        else if(objs.get(0).getClass() == Track.class)
+        {
+            TrackViewer tViewer = new TrackViewer(mainFrame, (List<Track>)objs, getPane((List<Track>)objs), false);
+            tViewer.setVisible(true);
+            System.out.println("Show track here");
+        }
+        else
+        {
+            CompViewer cViewer = new CompViewer(mainFrame, (List<Competition>)objs, getPane((List<Competition>)objs), false);
+            cViewer.setVisible(true);
+            System.out.println("Show competition here");
+        }
+    }
+
     class RacerViewer extends JDialog
     {
         private List<Racer> racers;
         private JPanel RV_panel;
 
-        public RacerViewer(JFrame owner, List<Racer>_racers)
+        public RacerViewer(JFrame owner, List<Racer>_racers, JScrollPane _racersPane, boolean editable)
         {
             super(owner, "Racers", false);
             this.setPreferredSize(new Dimension(650, 400));
             this.racers = _racers;
+            if(editable)
+                addMenu();
 
+            RV_panel = new JPanel();
+            RV_panel.add(_racersPane);
+            this.add(RV_panel);
+
+            this.pack();
+        }
+
+        private void addMenu()
+        {
             JMenuBar menubar = new JMenuBar();
             this.setJMenuBar(menubar);
 
@@ -304,12 +547,6 @@ class MainMenu
 
             racerMenu.add(removeRacer);
             this.setJMenuBar(menubar);
-
-            RV_panel = new JPanel();
-            RV_panel.add(racersPane);
-            this.add(RV_panel);
-
-            this.pack();
         }
     }
 
@@ -318,12 +555,24 @@ class MainMenu
         private List<Team> teams;
         private JPanel TV_panel;
 
-        public TeamViewer(JFrame owner, List<Team>_teams)
+        public TeamViewer(JFrame owner, List<Team>_teams, JScrollPane _teamsPane, boolean editable)
         {
             super(owner, "Teams", false);
             this.setPreferredSize(new Dimension(550, 400));
             this.teams = _teams;
 
+            if(editable)
+                addMenu();
+
+            TV_panel = new JPanel();
+            TV_panel.add(_teamsPane);
+            this.add(TV_panel);
+
+            this.pack();
+        }
+
+        private void addMenu()
+        {
             JMenuBar menubar = new JMenuBar();
             this.setJMenuBar(menubar);
 
@@ -359,12 +608,6 @@ class MainMenu
             teamMenu.add(removeTeam);
 
             this.setJMenuBar(menubar);
-
-            TV_panel = new JPanel();
-            TV_panel.add(teamsPane);
-            this.add(TV_panel);
-
-            this.pack();
         }
     }
 
@@ -373,12 +616,24 @@ class MainMenu
         private List<Track> tracks;
         private JPanel TeamV_panel;
 
-        public TrackViewer(JFrame owner, List<Track>_tracks)
+        public TrackViewer(JFrame owner, List<Track>_tracks, JScrollPane _tracksPane, boolean editable)
         {
             super(owner, "Tracks", false);
             this.setPreferredSize(new Dimension(550, 400));
             this.tracks = _tracks;
+            
+            if(editable)
+                addMenu();
 
+            TeamV_panel = new JPanel();
+            TeamV_panel.add(_tracksPane);
+            this.add(TeamV_panel);
+
+            this.pack();
+        }
+
+        private void addMenu()
+        {
             JMenuBar menubar = new JMenuBar();
             this.setJMenuBar(menubar);
 
@@ -414,12 +669,6 @@ class MainMenu
             trackMenu.add(removeTrack);
 
             this.setJMenuBar(menubar);
-
-            TeamV_panel = new JPanel();
-            TeamV_panel.add(tracksPane);
-            this.add(TeamV_panel);
-
-            this.pack();
         }
     }
 
@@ -428,12 +677,24 @@ class MainMenu
         private List<Competition> comps;
         private JPanel CV_panel;
 
-        public CompViewer(JFrame owner, List<Competition>_comps)
+        public CompViewer(JFrame owner, List<Competition>_comps, JScrollPane _compsPane, boolean editable)
         {
             super(owner, "Competitions", false);
             this.setPreferredSize(new Dimension(800, 400));
             this.comps = _comps;
+            
+            if(editable)
+                addMenu();
 
+            CV_panel = new JPanel();
+            CV_panel.add(_compsPane);
+            this.add(CV_panel);
+
+            this.pack();
+        }
+
+        private void addMenu()
+        {
             JMenuBar menubar = new JMenuBar();
             this.setJMenuBar(menubar);
 
@@ -469,12 +730,6 @@ class MainMenu
             compMenu.add(removeCompetition);
 
             this.setJMenuBar(menubar);
-
-            CV_panel = new JPanel();
-            CV_panel.add(compsPane);
-            this.add(CV_panel);
-
-            this.pack();
         }
     }
 }
